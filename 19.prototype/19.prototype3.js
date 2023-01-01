c8006c9e18953545263c08983b1a82278fdd3beb
corgiccori97 <tkdgmlgg7@gmail.com>
2023-01-01 22:35:42 +0900

    230101

--- 19.prototype/19.prototype3.js
+++ 19.prototype/19.prototype3.js
@@ -19,4 +19,26 @@ const me = new Person('KIM');
 
 Person.staticMethod(); // static method
 
-me.staticMethod(); // Uncaught TypeError TypeError: me.staticMethod is not a function
+// me.staticMethod(); // Uncaught TypeError TypeError: me.staticMethod is not a function
+
+console.log('-----프로퍼티 열거-----');
+const arr = [1, 2, 3];
+arr.x = 10;
+
+// ㉠
+for (const i in arr) {
+    console.log(arr[i]); // 1 2 3 10
+};
+
+// ㉡ arr.length에 따라 출력
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]); // 1 2 3
+}
+
+// ㉢ 요소가 아닌 프로퍼티 제외
+arr.forEach(v => console.log(v)); // 1 2 3
+
+// ㉣ 변수 선언문에서 선언한 변수에 키가 아닌 값을 할당
+for (const value of arr) {
+    console.log(value);
+};
