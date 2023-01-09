34b1b18c6899b7d89b4b5f3204896f88e7e3c6b4
corgiccori97 <tkdgmlgg7@gmail.com>
2023-01-10 02:39:23 +0900

    0110

--- 21.built-in-object/21.bult-in-object.js
+++ 21.built-in-object/21.bult-in-object.js
@@ -3,7 +3,7 @@ const numObj = new Number(1.5);
 console.log(numObj.toFixed()); // 2
 console.log(Number.isInteger(0.5)); // false
 
-console.log*('-----2. 원시값과 래퍼 객체-----');
+console.log('-----2. 원시값과 래퍼 객체-----');
 // ㉠
 const str = 'hello';
 console.log(str.length); // 5
