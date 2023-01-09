c07e067a61204a552ed826c665d2d9b91d256c68
corgiccori97 <tkdgmlgg7@gmail.com>
2023-01-09 23:03:29 +0900

    0109

---
+++ 21.built-in-object/21.bult-in-object.js
@@ -0,0 +1,21 @@
+const numObj = new Number(1.5);
+
+console.log(numObj.toFixed()); // 2
+console.log(Number.isInteger(0.5)); // false
+
+console.log*('-----2. 원시값과 래퍼 객체-----');
+// ㉠
+const str = 'hello';
+console.log(str.length); // 5
+console.log(str.toUpperCase()); // HELLO
+
+// ㉡
+str.name = 'Kim';
+
+// ㉢
+
+// ㉣
+console.log(str.name); // undefined
+
+// ㉤
+console.log(typeof str, str); // string hello 
