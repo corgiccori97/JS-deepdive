14572581b072593b33f970dbaaf3144714ab7ff4
corgiccori97 <tkdgmlgg7@gmail.com>
2023-01-17 23:46:47 +0900

    0117

--- 22.this/22.this.js
+++ 22.this/22.this.js
@@ -7,3 +7,28 @@ Circle.prototype.getDiameter = function() {
 };
 
 const circle = new Circle(5);
+
+console.log('-----this 바인딩(일반 함수)-----');
+
+var value = 1;
+
+const obj = {
+    value: 100,
+    foo() {
+        setTimeout(() => console.log(this.value), 100); // 100
+    }
+};
+
+obj.foo();
+
+console.log('-----this 바인딩(간접 호출)-----');
+
+function getThisBinding() {
+    console.log(arguments);
+    return this;
+} 
+
+const thisArg = { a : 1 };
+
+console.log(getThisBinding.apply(thisArg, [1, 2, 3]));
+console.log(getThisBinding.call(thisArg, 1, 2, 3));
